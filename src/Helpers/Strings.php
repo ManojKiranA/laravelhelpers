db8eb97fe12adfcabc11977954663276066d28aa
Manojkiran Appathurai <manojkiran.a@Postiefs.com>
2019-03-25 13:30:08 +0530

    marked between as static method

--- src/Helpers/Strings.php
+++ src/Helpers/Strings.php
@@ -5,28 +5,28 @@ namespace Manojkiran\LaravelHelpers\Helpers;
 class StringHelper
 {
     /**
- * Extracts the string between two substrings
- *
- * @param string $input
- * @param string $onLeft
- * @param string $onRight
- *
- * @return string
- *
- * @author Manojkiran.A <manojkiran10031998@gmail.com>
- */
-    function between($input='', $onLeft='', $onRight='')
+     * Extracts the string between two substrings
+     *
+     * @param string $inputString
+     * @param string $onLeft
+     * @param string $onRight
+     *
+     * @return string
+     *
+     * @author Manojkiran.A <manojkiran10031998@gmail.com>
+     */
+    public static function between(string $inputString,string $onLeft, string $onRight)
     {
-        $input = ' ' . $input;
-        $ini   = strpos($input, $onLeft);
+        $inputString = ' ' . $inputString;
+        $ini   = strpos( $inputString, $onLeft);
 
         if ($ini == 0) {
             return '';
         }
 
         $ini += strlen( $onLeft);
-        $len = strpos($input, $onRight, $ini) - $ini;
+        $len = strpos( $inputString, $onRight, $ini) - $ini;
 
-        return substr($input, $ini, $len);
-}
+        return substr( $inputString, $ini, $len);
+    }   
 }
