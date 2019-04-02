6344ae7f3081220fbba29eb2bbae25c8ee2277d3
Manojkiran Appathurai <manojkiran.a@Postiefs.com>
2019-04-02 14:52:42 +0530

    added info

--- src/Helpers/DateHelper.php
+++ src/Helpers/DateHelper.php
@@ -1,7 +1,12 @@
 <?php
 
 namespace Manojkiran\LaravelHelpers\Helpers;
-
+/**
+ * useful Date helpers for php and laravel or other frameworks
+ *
+ * @author [A. Manojkiran] [<manojkiran10031998@gmail.com>]
+ * @since      1.0.8
+ */
 class DateHelper
 {
 
